cfdb195bbf0dce49ced2586b47c974cc82f40c90
jamesJong99 <jamesjong99@gmail.com>
2025-08-27 12:41:42 -0700

    changed all files

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import { AiOutlineTool } from "react-icons/ai";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
+
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with the following technologies in the web development world. Now I'm focused towards React and Next.js.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="6rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            HTML, CSS, JavaScript, React, Next.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="6rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Next.js, Wordpress
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiZend size="6rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            PS, XD, Zeplin, Figma, Avocode
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <AiOutlineTool size="5.5rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Other Tools</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            WordPress CMS, Jira, Trello, Git
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,267 @@
+export const projects = [
+  {
+    title: "Click Colour",
+    description:
+      "What a frontend developer doesn't build up a similar application? This app uses custom CSS properties for the UI and only pure JavaScript code to change color clicking get color button. Simple as that!",
+    image: "images/jpgs/hex-color-1.jpg",
+    imageWebp: "images/webps/hex-color-1.webp",
+    images: [
+      {
+        original: "images/projects/ClickColour/hex-color-1.jpg",
+        loading: "lazy",
+        originalAlt: "hex-color-1",
+      },
+      {
+        original: "images/projects/ClickColour/hex-color-2.jpg",
+        loading: "lazy",
+        originalAlt: "hex-color-2",
+      },
+    ],
+    tags: ["JavaScript", "CSS", "HTML"],
+    id: 1,
+    order: 100,
+    mainType: "javascript",
+  },
+  {
+    title: "Sleet",
+    description:
+      "This is a WordPress starter theme called sleet. A theme meant for beginning theme development from scratch with a modern workflow at that time, in February 2021.",
+    image: "images/jpgs/sleet.jpg",
+    imageWebp: "images/webps/sleet.webp",
+    images: [
+      {
+        original: "images/projects/sleet.webp",
+        loading: "lazy",
+        originalAlt: "sleet",
+      }
+    ],
+    tags: ["PHP", "WordPress", "JavaScript", "HTML", "SCSS"],
+    id: 6,
+    order: 98,
+    mainType: "php",
+  },
+  {
+    title: "React Todo/Blog",
+    description:
+      "When I have commenced to learn React JS, I built this nice and cool React application. It combines a Todo and blog components as well as contact. The whole stack is pointed below.",
+    image: "images/jpgs/React-todo-and-blog-home-1.jpg",
+    imageWebp: "images/webps/React-todo-and-blog-home-1.webp",
+    images: [
+      {
+        original:
+          "images/projects/ReactTodoBlog/React-todo-and-blog-home-1.jpg",
+        loading: "lazy",
+        originalAlt: "React-todo-and-blog-home-1",
+      },
+      {
+        original:
+          "images/projects/ReactTodoBlog/React-todo-and-blog-blog-1.jpg",
+        loading: "lazy",
+        originalAlt: "React-todo-and-blog-blog-1",
+      },
+      {
+        original:
+          "images/projects/ReactTodoBlog/React-todo-and-blog-blog-2.jpg",
+        loading: "lazy",
+        originalAlt: "React-todo-and-blog-blog-2",
+      },
+      {
+        original:
+          "images/projects/ReactTodoBlog/React-todo-and-blog-contact.jpg",
+        loading: "lazy",
+        originalAlt: "React-todo-and-blog-contact",
+      },
+    ],
+    tags: [
+      "React",
+      "React hooks",
+      "React Hook Form",
+      "React transition group",
+      "React router",
+      "React Modal",
+      "React Query",
+      "Axios",
+      "Reddit API",
+      "localstorage",
+    ],
+    id: 4,
+    order: 94,
+    mainType: "react",
+  },
+  {
+    title: "Cryptocurrency",
+    description:
+      "Cryptocurrency app is my best React application so far. It's built up using Ant design, RTK Query, ChartJS, Coinranking and Bing News API. I have learnt a lot of interesting technologies like RTK Query, ChartJS as well as integrating custom features into Ant components like filter/sorting data, view blocks as list/grid or implementing load more component.",
+    image: "images/jpgs/ReactCryptocurrencyApp.jpg",
+    imageWebp: "images/webps/ReactCryptocurrencyApp.webp",
+    images: [
+      {
+        original:
+          "images/projects/ReactCryptocurrency/ReactCryptocurrencyApp.jpg",
+        loading: "lazy",
+        originalAlt: "React-Cryptocurrency",
+      },
+      {
+        original:
+          "images/projects/ReactCryptocurrency/ReactCryptocurrencyAppHome.jpg",
+        loading: "lazy",
+        originalAlt: "React-Cryptocurrency-Home",
+      },
+      {
+        original:
+          "images/projects/ReactCryptocurrency/ReactCryptocurrencyAppCryptocurrencies.jpg",
+        loading: "lazy",
+        originalAlt: "React--Cryptocurrency-Cryptocurrencies",
+      },
+      {
+        original:
+          "images/projects/ReactCryptocurrency/ReactCryptocurrencyAppExchanges.jpg",
+        loading: "lazy",
+        originalAlt: "React-Cryptocurrency-Exchanges",
+      },
+      {
+        original:
+          "images/projects/ReactCryptocurrency/ReactCryptocurrencyAppNews.jpg",
+        loading: "lazy",
+        originalAlt: "React-Cryptocurrency-News",
+      },
+    ],
+    tags: [
+      "React",
+      "React Hooks",
+      "Redux",
+      "RTK Query",
+      "Rapid API",
+      "Ant Design",
+      "ChartJS",
+    ],
+    id: 7,
+    order: 93,
+    mainType: "react",
+  },
+  {
+    title: "RLibrary",
+    description:
+      "Simple CRUD books system using Firebase/Firestore, Material UI, React, React Hook Form, React Router v6 and all these things bundled by vite",
+    image: "images/jpgs/ReactLibraryHomeDark.jpg",
+    imageWebp: "images/webps/ReactLibraryHomeDark.webp",
+    images: [
+      {
+        original: "images/projects/Rlibrary/ReactLibraryHomeDark.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary Home Dark",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryHomeLight.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary Home Light",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryHomeSignUpErrors.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary Home Sign uo Errors",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryViewLight.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary View Light",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryViewAddNewBook.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary View Add New Book",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryViewEditBook.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary View Edit Book",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryViewDeleteBook.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary View Delete Book",
+      },
+      {
+        original: "images/projects/Rlibrary/ReactLibraryViewMobile.jpg",
+        loading: "lazy",
+        originalAlt: "RLibrary View Mobile",
+      },
+    ],
+    tags: [
+      "React",
+      "React hooks",
+      "Context API",
+      "React Suspense",
+      "Router DOM v6",
+      "Firebase/Firestore",
+      "Material UI",
+    ],
+    id: 9,
+    order: 91,
+    mainType: "react",
+  },
+  {
+    title: "Chat Room [Test]",
+    description:
+      "An interview test project that aims to verify skills in understanding the task and building UI with React.",
+    image: "images/jpgs/React-test Chat Room Intro dark.jpg",
+    imageWebp: "images/webps/React-test Chat Room Intro dark.webp",
+    images: [
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room Intro dark.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room Intro dark",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room Intro light.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room Intro light",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room User Chat dark.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room User Chat dark",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room User Chat light.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room User Chat light",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room User extra options dark.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room User extra options dark",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room User Info dark.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room User Info dark",
+      },
+      {
+        original: "images/projects/ReactTestChatRoom/React-test Chat Room User Info light.webp",
+        loading: "lazy",
+        originalAlt: "Chat Room User Info light",
+      },
+    ],
+    tags: [
+      "Typescript",
+      "Jotai",
+      "SWR",
+      "Tailwind CSS",
+      "Headless UI",
+      "json server",
+      "React Error Boundary",
+    ],
+    id: 12,
+    order: 88,
+    mainType: "react",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Starting my journey as a Junior web developer" },
+  { year: 2018, text: "Working as a front-end developer" },
+  { year: 2019, text: "Working as a full stack developer" },
+  { year: 2021, text: "Diving completely into ReactJS World" },
+  { year: 2022, text: "Starting my journey as a Next.js developer" },
+];
